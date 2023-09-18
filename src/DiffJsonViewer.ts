afebc2f87250e42fde0683c113ff33255be1e62a
Henning Dieterichs <notify.henning.dieterichs@live.de>
2023-09-18 15:26:18 +0200

    Updates monaco editor (#9)

--- src/DiffJsonViewer.ts
+++ src/DiffJsonViewer.ts
@@ -1,31 +1,31 @@
 import * as vscode from "vscode";
 
 export class DiffJsonViewer {
-    constructor(context: vscode.ExtensionContext) {
-        vscode.window.registerCustomEditorProvider(
-            "diffJsonViewer",
-            {
-                async resolveCustomTextEditor(document, webviewPanel, _token) {
-                    const webviewRoot = vscode.Uri.joinPath(
-                        context.extensionUri,
-                        "webview"
-                    );
-                    webviewPanel.webview.options = {
-                        enableScripts: true,
-                        localResourceRoots: [webviewRoot],
-                    };
-    
-                    const script = webviewPanel.webview.asWebviewUri(
-                        vscode.Uri.joinPath(webviewRoot, "/src/script.js")
-                    );
-                    const monacoEditorCore = webviewPanel.webview.asWebviewUri(
-                        vscode.Uri.joinPath(
-                            webviewRoot,
-                            "/node_modules/monaco-editor-core/"
-                        )
-                    );
-    
-                    webviewPanel.webview.html = `
+	constructor(context: vscode.ExtensionContext) {
+		vscode.window.registerCustomEditorProvider(
+			"diffJsonViewer",
+			{
+				async resolveCustomTextEditor(document, webviewPanel, _token) {
+					const webviewRoot = vscode.Uri.joinPath(
+						context.extensionUri,
+						"webview"
+					);
+					webviewPanel.webview.options = {
+						enableScripts: true,
+						localResourceRoots: [webviewRoot],
+					};
+
+					const script = webviewPanel.webview.asWebviewUri(
+						vscode.Uri.joinPath(webviewRoot, "/src/script.js")
+					);
+					const monacoEditorCore = webviewPanel.webview.asWebviewUri(
+						vscode.Uri.joinPath(
+							webviewRoot,
+							"/node_modules/monaco-editor/"
+						)
+					);
+
+					webviewPanel.webview.html = `
                     <style>
                         body, html {
                             height: 100%;
@@ -37,80 +37,103 @@ export class DiffJsonViewer {
                     </script>
                     <script src="${script}"></script>
                 `;
-    
-                    interface DiffJsonDocument {
-                        diffs: unknown;
-                        originalFileName: string | undefined;
-                        modifiedFileName: string | undefined;
-    
-                        original: { content: string } | undefined;
-                        modified: { content: string } | undefined;
-                    }
-    
-                    async function update() {
-                        const doc = JSON.parse(
-                            document.getText()
-                        ) as DiffJsonDocument;
-    
-                        let originalDocument = "";
-                        let modifiedDocument = "";
-    
-                        if (doc.originalFileName) {
-                            const modifiedDoc =
-                                await vscode.workspace.openTextDocument(
-                                    vscode.Uri.joinPath(
-                                        document.uri,
-                                        "..",
-                                        doc.modifiedFileName!
-                                    )
-                                );
-                            const originalDoc =
-                                await vscode.workspace.openTextDocument(
-                                    vscode.Uri.joinPath(
-                                        document.uri,
-                                        "..",
-                                        doc.originalFileName!
-                                    )
-                                );
-                            originalDocument = originalDoc.getText();
-                            modifiedDocument = modifiedDoc.getText();
-                        } else {
-                            originalDocument = doc.original!.content;
-                            modifiedDocument = doc.modified!.content;
-                        }
-    
-                        await webviewPanel.webview.postMessage({
-                            type: "loadData",
-                            originalDocument: originalDocument,
-                            modifiedDocument: modifiedDocument,
-                            diffs: doc.diffs,
-                        });
-                    }
-    
-                    const subscription = vscode.workspace.onDidChangeTextDocument(
-                        (e) => {
-                            if (e.document === document) {
-                                update();
-                            }
-                        }
-                    );
-                    webviewPanel.onDidDispose(() => {
-                        subscription.dispose();
-                    });
-    
-                    webviewPanel.webview.onDidReceiveMessage(async (message) => {
-                        const m = message as { type: "ready" };
-    
-                        if (m.type === "ready") {
-                            update();
-                        }
-                    });
-                },
-            },
-            {
-                supportsMultipleEditorsPerDocument: true,
-                webviewOptions: { retainContextWhenHidden: true },
-            }
-        );
-    }
+
+					interface DiffJsonDocument {
+						diffs: unknown;
+						languageId: string;
+						originalFileName: string | undefined;
+						modifiedFileName: string | undefined;
+
+						original:
+							| { content: string; fileName: string }
+							| undefined;
+						modified:
+							| { content: string; fileName: string }
+							| undefined;
+					}
+
+					async function update() {
+						const doc = JSON.parse(
+							document.getText()
+						) as DiffJsonDocument;
+
+						let originalDocument = "";
+						let modifiedDocument = "";
+						let originalPath: string | undefined;
+
+						if (doc.originalFileName) {
+							originalPath = doc.originalFileName;
+							const modifiedDoc =
+								await vscode.workspace.openTextDocument(
+									vscode.Uri.joinPath(
+										document.uri,
+										"..",
+										doc.modifiedFileName!
+									)
+								);
+							const originalDoc =
+								await vscode.workspace.openTextDocument(
+									vscode.Uri.joinPath(
+										document.uri,
+										"..",
+										doc.originalFileName!
+									)
+								);
+							originalDocument = originalDoc.getText();
+							modifiedDocument = modifiedDoc.getText();
+						} else {
+							originalPath = doc.original?.fileName;
+							originalDocument = doc.original!.content;
+							modifiedDocument = doc.modified!.content;
+						}
+
+						function guessLanguage(path: string): string {
+							const ext = path.split(".").pop();
+							if (ext === "js") {
+								return "javascript";
+							}
+							if (ext === "tst") {
+								return "typescript";
+							}
+							return "text";
+						}
+
+						await webviewPanel.webview.postMessage({
+							type: "loadData",
+							originalDocument: originalDocument,
+							modifiedDocument: modifiedDocument,
+							diffs: doc.diffs,
+							languageId: originalPath
+								? guessLanguage(originalPath)
+								: undefined,
+						});
+					}
+
+					const subscription =
+						vscode.workspace.onDidChangeTextDocument((e) => {
+							if (e.document === document) {
+								update();
+							}
+						});
+					webviewPanel.onDidDispose(() => {
+						subscription.dispose();
+					});
+
+					webviewPanel.webview.onDidReceiveMessage(
+						async (message) => {
+							const m = message as { type: "ready" };
+
+							if (m.type === "ready") {
+								update();
+							}
+						}
+					);
+				},
+			},
+			{
+				supportsMultipleEditorsPerDocument: true,
+				webviewOptions: { retainContextWhenHidden: true },
+			}
+		);
+	}
 }
--- webview/src/script.js
+++ webview/src/script.js
@@ -1,129 +1,149 @@
 require(['vs/editor/editor.main'], function () {
-    document.body.innerHTML = `
-        <div style="width: 100%; height: 100%; display: flex; flex-direction: column;">
-            <span>LCS Length: <span id="lcsText">10</span></span>
-            <div id="container" style="flex: 1"></div>
-        </div>
-    `;
+	document.body.innerHTML = `
+		<div style="width: 100%; height: 100%; display: flex; flex-direction: column;">
+			<span>LCS Length: <span id="lcsText">10</span></span>
+			<div id="container" style="flex: 1"></div>
+		</div>
+	`;
 
-    /**
-     * @type {import("monaco-editor-core")}
-     */
-    const m = monaco;
+	/**
+	 * @type {import("monaco-editor")}
+	 */
+	const m = monaco;
 
-    const changeEmitter = new m.Emitter();
-    /**
-     * @type import("monaco-editor-core").editor.LineRangeMapping[]
-     */
-    let changes = [];
+	class MyDiffProviderFactoryService {
+		createDiffProvider(editor, options) {
+			return {
+				onDidChange: changeEmitter.event,
+				async computeDiff(original, modified, options, token) {
+					let d = {
+						identical: false,
+						quitEarly: false,
+						changes: changes,
+					};
+					return d;
+				}
+			}
+		}
+	}
+	const standaloneServices = require("vs/editor/standalone/browser/standaloneServices");
+	standaloneServices.StandaloneServices.initialize({
+		'diffProviderFactoryService': new MyDiffProviderFactoryService(),
+	});
 
-    const diffEditor = m.editor.createDiffEditor(document.getElementById('container'), {
-        automaticLayout: true,
-        ignoreTrimWhitespace: false,
-        experimental: {
-            //useVersion2: true, TODO@hediet: enable this when customizing the diff algorithm is possible.
-        },
-        diffAlgorithm: {
-            onDidChange: changeEmitter.event,
-            computeDiff: () => {
-                let d = {
-					identical: false,
-					quitEarly: false,
-					changes: changes,
-				};
-                console.log('compute');
-				return d;
-            }
-        },
-    });
 
-    const originalModel = m.editor.createModel('', 'text');
-    const modifiedModel = m.editor.createModel('', 'text');
+	const changeEmitter = new m.Emitter();
+	/**
+	 * @type import("monaco-editor").editor.LineRangeMapping[]
+	 */
+	let changes = [];
 
-    diffEditor.setModel({
-        original: originalModel,
-        modified: modifiedModel
-    });
+	const diffEditor = m.editor.createDiffEditor(document.getElementById('container'), {
+		automaticLayout: true,
+		ignoreTrimWhitespace: false,
+		hideUnchangedRegions: {
+			enabled: true,
+		},
+	});
 
-    const api = acquireVsCodeApi();
-    api.postMessage({
-        type: 'ready'
-    });
+	m.languages.typescript.typescriptDefaults.setDiagnosticsOptions({ noSemanticValidation: true, noSyntaxValidation: true });
+	m.languages.typescript.javascriptDefaults.setDiagnosticsOptions({ noSemanticValidation: true, noSyntaxValidation: true });
 
-    window.addEventListener('message', e => {
-        /**
-         * @type {{
-         *    kind: 'loadData';
-         *    originalDocument: string;
-         *    modifiedDocument: string;
-         *    diffs: {
-         *          originalRange: string;
-         *          modifiedRange: string;
-         *          innerChanges: {
-         *              originalRange: string;
-         *              modifiedRange: string;
-         *          }[];
-         *    }[]
-         * }}
-         */
-        const message = e.data;
+	const originalModel = m.editor.createModel('', 'text');
+	const modifiedModel = m.editor.createModel('', 'text');
 
-        originalModel.setValue(message.originalDocument);
-        modifiedModel.setValue(message.modifiedDocument);
+	diffEditor.setModel({
+		original: originalModel,
+		modified: modifiedModel
+	});
 
-        function parseLineRange(str) {
-            const [start, end] = str.slice(1, -1).split(',');
-            return new m.editor.LineRange(+start, +end);
-        }
+	const api = acquireVsCodeApi();
+	api.postMessage({
+		type: 'ready'
+	});
 
-        function parseRange(str) {
-            const [startPos, endPos] = str.slice(1, -1).split('->');
-            const [startLineNumber, startColumn] = startPos.split(',');
-            const [endLineNumber, endColumn] = endPos.split(',');
-            
-            return new m.Range(
-                +startLineNumber,
-                +startColumn,
-                +endLineNumber,
-                +endColumn
-            );  
-        }
+	const { RangeMapping, DetailedLineRangeMapping } = require('vs/editor/common/diff/rangeMapping');
+	const { Range } = require('vs/editor/common/core/range');
+	const { LineRange } = require('vs/editor/common/core/lineRange');
+	
+	window.addEventListener('message', e => {
+		/**
+		 * @type {{
+		 *    kind: 'loadData';
+		 *    originalDocument: string;
+		 *    modifiedDocument: string;
+		 *    languageId?: string;
+		 *    diffs: {
+		 *          originalRange: string;
+		 *          modifiedRange: string;
+		 *          innerChanges: {
+		 *              originalRange: string;
+		 *              modifiedRange: string;
+		 *          }[];
+		 *    }[]
+		 * }}
+		 */
+		const message = e.data;
+		
+		originalModel.setValue(message.originalDocument);
+		modifiedModel.setValue(message.modifiedDocument);
+		console.log(message.languageId);
+		m.editor.setModelLanguage(originalModel, message.languageId ?? 'text');
+		m.editor.setModelLanguage(modifiedModel, message.languageId ?? 'text');
 
-        changes = message.diffs.map(d => new m.editor.LineRangeMapping(
-            parseLineRange(d.originalRange),
-            parseLineRange(d.modifiedRange),
-            d.innerChanges ? d.innerChanges.map(c => new m.editor.RangeMapping(
-                parseRange(c.originalRange),
-                parseRange(c.modifiedRange)
-            )) : undefined
-        ));
+		function parseLineRange(str) {
+			const [start, end] = str.slice(1, -1).split(',');
+			return new LineRange(+start, +end);
+		}
 
-        function lineRangeToRange(lineRange) {
-            return new m.Range(
-                lineRange.startLineNumber,
-                1,
-                lineRange.endLineNumber,
-                1
-            );
-        }
+		function parseRange(str) {
+			const [startPos, endPos] = str.slice(1, -1).split('->');
+			const [startLineNumber, startColumn] = startPos.split(',');
+			const [endLineNumber, endColumn] = endPos.split(',');
+			
+			return new Range(
+				+startLineNumber,
+				+startColumn,
+				+endLineNumber,
+				+endColumn
+			);  
+		}
 
-        const allInnerChanges = changes.flatMap(c => c.innerChanges || new m.editor.RangeMapping(lineRangeToRange(c.originalRange), lineRangeToRange(c.modifiedRange)));
-        let lcsLen = 0;
-        
-        for (let i = 0; i <= allInnerChanges.length; i++) {
-            const previousInnerChange = allInnerChanges[i - 1];
-            const innerChange = allInnerChanges[i];
-            
-            const startPosition = previousInnerChange ? previousInnerChange.originalRange.getEndPosition() : new m.Position(1, 1);
-            const endPosition = innerChange ? innerChange.originalRange.getStartPosition() : new m.Position(originalModel.getLineCount() + 1, 1);
+		changes = message.diffs.map(d => new DetailedLineRangeMapping(
+			parseLineRange(d.originalRange),
+			parseLineRange(d.modifiedRange),
+			d.innerChanges ? d.innerChanges.map(c => new RangeMapping(
+				parseRange(c.originalRange),
+				parseRange(c.modifiedRange)
+			)) : undefined
+		));
 
-            const range = m.Range.fromPositions(startPosition, endPosition);
-            const text = originalModel.getValueInRange(range);
-            lcsLen += text.length;
-        }
+		function lineRangeToRange(lineRange) {
+			return new Range(
+				lineRange.startLineNumber,
+				1,
+				lineRange.endLineNumber,
+				1
+			);
+		}
 
-        document.getElementById('lcsText').innerText = lcsLen.toString();
+		const allInnerChanges = changes.flatMap(c => c.innerChanges || new RangeMapping(lineRangeToRange(c.original), lineRangeToRange(c.modified)));
+		let lcsLen = 0;
+		
+		for (let i = 0; i <= allInnerChanges.length; i++) {
+			const previousInnerChange = allInnerChanges[i - 1];
+			const innerChange = allInnerChanges[i];
+			
+			const startPosition = previousInnerChange ? previousInnerChange.originalRange.getEndPosition() : new m.Position(1, 1);
+			const endPosition = innerChange ? innerChange.originalRange.getStartPosition() : new m.Position(originalModel.getLineCount() + 1, 1);
 
-        changeEmitter.fire();
-    });
+			const range = m.Range.fromPositions(startPosition, endPosition);
+			const text = originalModel.getValueInRange(range);
+			lcsLen += text.length;
+		}
+
+		document.getElementById('lcsText').innerText = lcsLen.toString();
+
+		changeEmitter.fire();
+	});
 });
